9a1f8a6fdbc3fb2f497ada7a649968aa5e3bb72e
Komissarov Eugene <komissarovea@hotmail.com>
2021-12-27 21:41:10 +0300

    First working alarm logic

--- android/app/src/main/kotlin/com/example/neat_alarm/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/neat_alarm/MainActivity.kt
@@ -1,6 +1,49 @@
 package com.example.neat_alarm
 
+import android.content.ContentResolver
+import android.content.Context
+import android.media.RingtoneManager
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import java.util.*
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "kea.dev/neat_alarm").setMethodCallHandler { call, result ->
+            if ("drawableToUri" == call.method) {
+                val resourceId = this@MainActivity.resources.getIdentifier(call.arguments as String, "drawable", this@MainActivity.packageName)
+                result.success(resourceToUriString(this@MainActivity.applicationContext, resourceId))
+            }
+            if ("getAlarmUri" == call.method) {
+                result.success(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM).toString())
+            }
+            if ("getAllSounds" == call.method) {
+                val manager = RingtoneManager(this@MainActivity)
+                manager.setType(RingtoneManager.TYPE_RINGTONE)
+                val cursor = manager.cursor
+
+                val list = HashMap<String, String>()
+                while (cursor.moveToNext()) {
+                    val id = cursor.getString(RingtoneManager.ID_COLUMN_INDEX)
+                    val uri = cursor.getString(RingtoneManager.URI_COLUMN_INDEX)
+                    val title = cursor.getString(RingtoneManager.TITLE_COLUMN_INDEX)
+
+                    list.set(title, "$uri/$id")
+                }
+
+                result.success(list)
+            }
+        }
+    }
+
+    private fun resourceToUriString(context: Context, resId: Int): String? {
+        return (ContentResolver.SCHEME_ANDROID_RESOURCE + "://"
+                + context.resources.getResourcePackageName(resId)
+                + "/"
+                + context.resources.getResourceTypeName(resId)
+                + "/"
+                + context.resources.getResourceEntryName(resId))
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'dart:async';
+
 import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
+const MethodChannel platform = MethodChannel('kea.dev/neat_alarm');
 
 Future<void> main() async {
   // needed if you intend to initialize in the `main` function
@@ -15,19 +21,19 @@ Future<void> main() async {
     debugPrint('from background');
   }
 
-  // const AndroidInitializationSettings initializationSettingsAndroid =
-  //     AndroidInitializationSettings('app_icon');
-  // const InitializationSettings initializationSettings =
-  //     InitializationSettings();
-  // // InitializationSettings(android: initializationSettingsAndroid);
-  // await flutterLocalNotificationsPlugin.initialize(initializationSettings,
-  //     onSelectNotification: (String? payload) async {
-  //   if (payload != null) {
-  //     debugPrint('notification payload: $payload');
-  //   }
-  //   //selectedNotificationPayload = payload;
-  //   //selectNotificationSubject.add(payload);
-  // });
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('app_icon');
+  const InitializationSettings initializationSettings =
+      InitializationSettings(android: initializationSettingsAndroid);
+  tz.initializeTimeZones();
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+      onSelectNotification: (String? payload) async {
+    if (payload != null) {
+      debugPrint('notification payload: $payload');
+    }
+    //selectedNotificationPayload = payload;
+    //selectNotificationSubject.add(payload);
+  });
 
   runApp(const MainApp());
 }
@@ -40,19 +46,76 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
+  static String channelId = UniqueKey().toString();
+  static const String applicationName = "Neat 1";
+
   @override
   Widget build(BuildContext context) {
     AppBar appBarWdg = AppBar(title: const Text('Neat Alarm'));
 
     var btnAddAlarm = ElevatedButton(
         child: const Text(
-          'Add New Alarm',
+          'Add New Alarm 5',
           style: TextStyle(fontSize: 18),
         ),
-        onPressed: () {
+        onPressed: () async {
           DateTime now = DateTime.now();
-          String formattedDate = DateFormat('yyyy-MM-dd HH:mm').format(now);
+          String formattedDate = DateFormat('yyyy-MM-dd HH:mm:ss').format(now);
           debugPrint('Clicked $formattedDate');
+
+          var lines = <String>['line <b>1</b>', 'line <i>2</i>'];
+          var inboxStyleInformation = InboxStyleInformation(lines,
+              htmlFormatLines: true,
+              contentTitle: 'overridden <b>inbox</b> context title',
+              htmlFormatContentTitle: true,
+              summaryText: 'summary <i>text</i>',
+              htmlFormatSummaryText: true);
+
+          String? alarmUri = await platform.invokeMethod<String>('getAlarmUri');
+          debugPrint('URI!!!!!!! $alarmUri');
+          // content://settings/system/alarm_alert
+          Map<Object?, Object?>? data = await platform
+              .invokeMethod<Map<Object?, Object?>>('getAllSounds');
+          //Map<String, String>? fullMap = Map<String, String>.from(data);
+          // debugPrint('$data');
+          data?.forEach((key, value) {
+            debugPrint('$key : $value');
+          });
+
+          alarmUri = 'content://media/external/audio/media/26242';
+          final UriAndroidNotificationSound uriSound =
+              UriAndroidNotificationSound(alarmUri); //!);
+
+          var platformChannelSpecifics = NotificationDetails(
+            android: AndroidNotificationDetails(
+              channelId,
+              applicationName,
+              channelDescription: 'Description 2',
+              importance: Importance.max,
+              playSound: true,
+              priority: Priority.high,
+              sound: uriSound,
+              //const RawResourceAndroidNotificationSound('mari'),
+              styleInformation: inboxStyleInformation,
+            ),
+          );
+
+          // await flutterLocalNotificationsPlugin.show(
+          //     12345,
+          //     "A Notification From My Application",
+          //     "This notification was sent using Flutter Local Notifcations Package",
+          //     platformChannelSpecifics,
+          //     payload: 'data');
+
+          await flutterLocalNotificationsPlugin.zonedSchedule(
+              12345,
+              "A Notification From My App",
+              "This notification is brought to you by Local Notifcations Package",
+              tz.TZDateTime.now(tz.local).add(const Duration(hours: 9)),
+              platformChannelSpecifics,
+              androidAllowWhileIdle: true,
+              uiLocalNotificationDateInterpretation:
+                  UILocalNotificationDateInterpretation.absoluteTime);
         });
     var btnContainer = Container(
         margin: const EdgeInsets.all(10),
